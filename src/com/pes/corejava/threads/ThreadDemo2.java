9fd2ed005f5c1ca9e08fba905e4f8d34b163cf17
Pushkar524 <pushkar552244@gmail.com>
2026-07-13 14:16:02 +0530

    Certai changes are made

--- src/com/pes/corejava/threads/ThreadDemo2.java
+++ src/com/pes/corejava/threads/ThreadDemo2.java
@@ -1,6 +1,6 @@
 package com.pes.corejava.threads;
 
-class MyClass implements Runnable{
+class myClass implements Runnable{
 	public void run() {
 		for(int i  =0 ;i <= 100 ; i++) {
 			System.out.println(i+" ");
@@ -9,7 +9,7 @@ class MyClass implements Runnable{
 }
 public class ThreadDemo2 {
 public static void main(String args[]) {
-	MyClass m1 = new MyClass();
+	myClass m1 = new myClass();
 	Thread t1 = new Thread(m1);
 	t1.start();
 	Thread t2 = new Thread(m1);
